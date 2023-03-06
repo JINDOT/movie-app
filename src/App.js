9319f4bee84916347f39f21b3200bd384778982d
JINDOT <jinwon1848@gmail.com>
2023-03-06 16:32:10 +0900

    0306

--- src/App.js
+++ src/App.js
@@ -1,57 +1,29 @@
-import { useEffect, useState, useSyncExternalStore } from 'react';
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  // then 다른 방법
-  const getMovies = async () => {
-    const json = await (
-      await fetch(
-        'https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year'
-      )
-    ).json();
-
-    setMovies(json.data.movies);
-    setLoading(false);
-  };
-  /*
-  useEffect(() => {
-    fetch(
-      'https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year'
-    ).then((response) => response.json())
-      .then((json) => {
-        setMovies(json.data.movies)
-        setLoading(false);
-      })
-  }, []);
-*/
-  useEffect(() => {
-    getMovies();
-  }, []);
   return (
-    <div>
-      {loading ? (<h1>Loading...</h1>) :
-        (
-          <div>
-            {movies.map((movie) => (
-              <div key={movie.id}>
-                <img src={movie.medium_cover_image} />
-                <h2>{movie.title}</h2>
-                <p>{movie.summary}</p>
-                <ul>
-                  {movie.genres.map((g) => (
-                    <li key={g}>{g}</li>
-                  ))}
-                </ul>
-              </div>
-            ))}
-          </div>
-        )}
-    </div>
+    < Router >
+      < Routes >
+        <Route path="/" element={<Home />} />
+        <Route path="/movie" element={<Detail />} />
+      </Routes >
+    </Router>
   );
-}
+};
 
 export default App;
+
 /* 역시 code는 무작정 따라쓰지 않고, 뭔지 알고 써야한다.*/
 /* map() : 반복되는 component를 랜더링하기 위해 사용 */
+
+
+/*
+  path = "/"  >> Home으로 돌아감
+  paht = "/???" >> ??? 이동
+
+ url의 path를 정해주는 것. /의 경우 뒤에 주소를 
+ 없애고 element의 Home으로 넘어감
+
+*/
