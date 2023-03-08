ae0de5f8144cfaac9b83c719bef7b4277f5e1f9b
JINDOT <jinwon1848@gmail.com>
2023-03-08 16:37:20 +0900

    0308

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,12 +1,12 @@
 import { PropTypes } from "prop-types";
 import { Link } from "react-router-dom";
 
-function Movie({ medium_cover_image, title, summary, genres }) {
+function Movie({ id, medium_cover_image, title, summary, genres }) {
     return (
         <div>
             <img src={medium_cover_image} alt={title} />
             <h2>
-                <Link to="/movie">{title}</Link>
+                <Link to={`/movie/${id}`}>{title}</Link>
             </h2>
             <p>{summary}</p>
             <ul>
@@ -18,7 +18,8 @@ function Movie({ medium_cover_image, title, summary, genres }) {
     )
 }
 
-Movie.prototype = {
+Movie.propTypes = {
+    id: PropTypes.number.isRequired,
     medium_cover_image: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
@@ -30,3 +31,4 @@ export default Movie;
 // react router 사용
 // : page 전환에 사용
 // : npm install react-router-dom
+// :  npm install react-router-dom@6
